703765301cbd95930509d79f4d8acbe66c5072c3
Georgy Torquemada <darkmasters@yandex.ru>
2020-06-06 09:11:56 +0300

    fix: force https

--- config/app.php
+++ config/app.php
@@ -56,6 +56,8 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
+    'is_secure' => env('APP_SECURE', false),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
